e8f99aab5e5638faee7fe0c382af06a72f718863
jasondecode <jasonhattu@hotmail.com>
2020-01-10 11:22:34 +0100

    🔨 Dump saved products

---
+++ app/Console/Components/ProductHunt/StatsProductsCommand.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Console\Components\ProductHunt;
+
+use Illuminate\Console\Command;
+use App\Services\Scraper\Models\Entity;
+use App\Services\ProductHunt\Models\EntityProduct;
+
+class StatsProductsCommand extends Command
+{
+    /** @var string*/
+    protected $signature = 'producthunt:stats-products';
+
+    /** @var string */
+    protected $description = 'Get saved products';
+
+    public function __construct()
+    {
+        parent::__construct();
+    }
+    
+    public function handle(Entity $entity)
+    {        
+        $entity->all()
+            ->reverse()
+            ->filter(function ($entity) {
+                return $entity->entityable_type === EntityProduct::class;            
+            })
+            ->unique('entity_unique_code')
+            ->map(function ($entity) {
+                return [
+                    'name' => $entity->entityable->name,
+                    'votes' => $entity->entityable->votes,
+                    'featured_at' => $entity->entityable->featured_at
+                ];
+            })
+            ->sortByDesc('votes')
+            ->slice(0, 10)
+            ->dump();
+    }
+}
